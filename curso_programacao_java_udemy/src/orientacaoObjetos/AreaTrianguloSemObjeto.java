36954c039a59c0a4ea8b29eb68731026729421a3
Thiago <1thiago1henrique@gmail.com>
2024-05-22 22:53:52 -0300

    subindo aula fim do dia

---
+++ curso_programacao_java_udemy/src/orientacaoObjetos/AreaTrianguloSemObjeto.java
@@ -0,0 +1,36 @@
+package orientacaoObjetos;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class AreaTrianguloSemObjeto {
+
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        double xA, xB, xC, yA, yB, yC;
+        System.out.println("Enter the measures of triangle X: ");
+        xA = sc.nextDouble();
+        xB = sc.nextDouble();
+        xC = sc.nextDouble();
+        System.out.println("Enter the measures of triangle Y: ");
+        yA = sc.nextDouble();
+        yB = sc.nextDouble();
+        yC = sc.nextDouble();
+        
+        double p = (xA + xB + xC) / 2.0;
+        double areaX = Math.sqrt(p * (p - xA) * (p - xB) * (p - xC));
+        
+        p = (yA + yB + yC) / 2.0;
+        double areaY = Math.sqrt(p * (p - yA) * (p - yB) * (p - yC));
+        
+        System.out.printf("Triangle X area: %.4f%n", areaX);
+        System.out.printf("Triangle Y area: %.4f%n", areaY);
+        
+        String resultado = areaX > areaY ? "Larger area: X" : "Larger area: Y";
+        System.out.println(resultado);
+
+        sc.close();
+    }
+
+}
